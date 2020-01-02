4383d55c2f8424b49fd748b27d116acb6b53272c
jiangxi8899 <jiangxi8899@gmail.com>
2020-01-02 16:59:46 +0800

    Signed-off-by: jiangxi8899 <jiangxi8899@gmail.com>

--- spring cloud/day1/first/spring_cloud/order_service/src/main/java/cn/idealismus/order/controller/OrderController.java
+++ spring cloud/day1/first/spring_cloud/order_service/src/main/java/cn/idealismus/order/controller/OrderController.java
@@ -31,12 +31,34 @@ public class OrderController {
      */
     @Autowired
     private DiscoveryClient discoveryClient;
+
+    /**
+     *
+     * 参数，商品ID
+     *  通过订单系统，调用商品服务 根据ID 查询对象
+     *
+     *  开始基于ribbon形式调用远程微服务
+     *  1.使用 @LoadBalanced 声明 RestTemplate
+     *  2.使用服务名替换ip地址
+     */
+    
+    @RequestMapping(value = "/buy/{id}",method = RequestMethod.GET)
+    public Product findById (@PathVariable Integer id) {
+        //调用 discoveryClient的方法
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("service-product");
+
+        ServiceInstance server = serviceInstances.get(0);
+        //如何调用商品的服务呢？
+        return restTemplate.getForObject("http://service-product/product/"+id,Product.class);
+    }
+    
     
     /**
+     * 
      * 参数，商品ID
      *  通过订单系统，调用商品服务 根据ID 查询对象
      */
-    @RequestMapping(value = "/buy/{id}",method = RequestMethod.GET)
+    /*@RequestMapping(value = "/buy/{id}",method = RequestMethod.GET)
     public Product findById (@PathVariable Integer id) {
         //调用 discoveryClient的方法
         List<ServiceInstance> serviceInstances = discoveryClient.getInstances("service-product");
@@ -44,9 +66,10 @@ public class OrderController {
         for (ServiceInstance server : serviceInstances) {
             System.out.println(server);
         }
+        
         ServiceInstance server = serviceInstances.get(0);
         //如何调用商品的服务呢？
-        return restTemplate.getForObject("http://"+ server.getHost() + server.getPort() +"/product/1",Product.class);
-    }
+        return restTemplate.getForObject("http://"+ server.getHost() + server.getPort() +"/product/"+id,Product.class);
+    }*/
     
 }
--- spring cloud/day1/first/spring_cloud/product_service/src/main/java/cn/idealismus/product/controller/ProductController.java
+++ spring cloud/day1/first/spring_cloud/product_service/src/main/java/cn/idealismus/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package cn.idealismus.product.controller;
 import cn.idealismus.product.entity.Product;
 import cn.idealismus.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,9 +13,17 @@ public class ProductController {
     @Autowired
     private ProductService productService;
     
+    @Value("${server.port}")
+    private String port;
+    
+    @Value("${spring.cloud.client.ip-address}")
+    private String ip;
+    
     @RequestMapping(value = "/{id}",method = RequestMethod.GET)
     public Product finById (@PathVariable Integer id) {
-        return productService.getProductById(id);
+        Product product = productService.getProductById(id);
+        product.setProductName("访问地址为"+ip+":"+port);
+        return product;
     }
     
     @RequestMapping(value = "",method = RequestMethod.POST)
